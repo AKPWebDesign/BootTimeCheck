5553a1877130af6818c1b58b5ce660ec0b3258b0
Austin Peterson <austin@akpwebdesign.com>
2015-07-31 14:13:30 -0500

    Add printing support.

--- BootTimeCheck/Main.Designer.cs
+++ BootTimeCheck/Main.Designer.cs
@@ -28,13 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem4 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
-            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem5 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
-            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem6 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem1 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem2 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem3 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.avgLabel = new System.Windows.Forms.Label();
@@ -46,45 +46,55 @@
             this.improvementBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cbxBoots = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.barProgress = new System.Windows.Forms.ToolStripProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legendItem4.Color = System.Drawing.Color.DarkRed;
-            legendItem4.Name = "Poor";
-            legendItem5.Color = System.Drawing.Color.Yellow;
-            legendItem5.Name = "Fair";
-            legendItem6.Color = System.Drawing.Color.LimeGreen;
-            legendItem6.Name = "Good";
-            legend2.CustomItems.Add(legendItem4);
-            legend2.CustomItems.Add(legendItem5);
-            legend2.CustomItems.Add(legendItem6);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(12, 67);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legendItem1.Color = System.Drawing.Color.DarkRed;
+            legendItem1.Name = "Poor";
+            legendItem2.Color = System.Drawing.Color.Yellow;
+            legendItem2.Name = "Fair";
+            legendItem3.Color = System.Drawing.Color.LimeGreen;
+            legendItem3.Name = "Good";
+            legend1.CustomItems.Add(legendItem1);
+            legend1.CustomItems.Add(legendItem2);
+            legend1.CustomItems.Add(legendItem3);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 101);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            series2.IsValueShownAsLabel = true;
-            series2.IsVisibleInLegend = false;
-            series2.LabelBackColor = System.Drawing.Color.Black;
-            series2.LabelForeColor = System.Drawing.Color.White;
-            series2.Legend = "Legend1";
-            series2.Name = "Series";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(766, 456);
+            series1.ChartArea = "ChartArea1";
+            series1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.IsValueShownAsLabel = true;
+            series1.IsVisibleInLegend = false;
+            series1.LabelBackColor = System.Drawing.Color.Black;
+            series1.LabelForeColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Series";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(766, 437);
             this.chart1.TabIndex = 0;
-            title2.Name = "TableTitle";
-            title2.Text = "Historical Boot Times (in seconds)";
-            this.chart1.Titles.Add(title2);
+            title1.Name = "TableTitle";
+            title1.Text = "Historical Boot Times (in seconds)";
+            this.chart1.Titles.Add(title1);
             // 
             // avgLabel
             // 
             this.avgLabel.AutoSize = true;
-            this.avgLabel.Location = new System.Drawing.Point(13, 10);
+            this.avgLabel.Location = new System.Drawing.Point(13, 44);
             this.avgLabel.Name = "avgLabel";
             this.avgLabel.Size = new System.Drawing.Size(101, 13);
             this.avgLabel.TabIndex = 1;
@@ -93,7 +103,7 @@
             // avgBox
             // 
             this.avgBox.Enabled = false;
-            this.avgBox.Location = new System.Drawing.Point(120, 7);
+            this.avgBox.Location = new System.Drawing.Point(120, 41);
             this.avgBox.Name = "avgBox";
             this.avgBox.Size = new System.Drawing.Size(74, 20);
             this.avgBox.TabIndex = 2;
@@ -101,7 +111,7 @@
             // minBox
             // 
             this.minBox.Enabled = false;
-            this.minBox.Location = new System.Drawing.Point(405, 7);
+            this.minBox.Location = new System.Drawing.Point(405, 41);
             this.minBox.Name = "minBox";
             this.minBox.Size = new System.Drawing.Size(74, 20);
             this.minBox.TabIndex = 4;
@@ -109,7 +119,7 @@
             // minLabel
             // 
             this.minLabel.AutoSize = true;
-            this.minLabel.Location = new System.Drawing.Point(297, 10);
+            this.minLabel.Location = new System.Drawing.Point(297, 44);
             this.minLabel.Name = "minLabel";
             this.minLabel.Size = new System.Drawing.Size(102, 13);
             this.minLabel.TabIndex = 3;
@@ -118,7 +128,7 @@
             // maxBox
             // 
             this.maxBox.Enabled = false;
-            this.maxBox.Location = new System.Drawing.Point(704, 7);
+            this.maxBox.Location = new System.Drawing.Point(704, 41);
             this.maxBox.Name = "maxBox";
             this.maxBox.Size = new System.Drawing.Size(74, 20);
             this.maxBox.TabIndex = 6;
@@ -126,7 +136,7 @@
             // maxLabel
             // 
             this.maxLabel.AutoSize = true;
-            this.maxLabel.Location = new System.Drawing.Point(593, 10);
+            this.maxLabel.Location = new System.Drawing.Point(593, 44);
             this.maxLabel.Name = "maxLabel";
             this.maxLabel.Size = new System.Drawing.Size(105, 13);
             this.maxLabel.TabIndex = 5;
@@ -135,7 +145,7 @@
             // improvementBox
             // 
             this.improvementBox.Enabled = false;
-            this.improvementBox.Location = new System.Drawing.Point(624, 39);
+            this.improvementBox.Location = new System.Drawing.Point(624, 73);
             this.improvementBox.Name = "improvementBox";
             this.improvementBox.Size = new System.Drawing.Size(74, 20);
             this.improvementBox.TabIndex = 8;
@@ -143,7 +153,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(381, 42);
+            this.label1.Location = new System.Drawing.Point(381, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(110, 13);
             this.label1.TabIndex = 7;
@@ -160,17 +170,82 @@
             "15 boots",
             "20 boots",
             "all boots"});
-            this.cbxBoots.Location = new System.Drawing.Point(497, 39);
+            this.cbxBoots.Location = new System.Drawing.Point(497, 73);
             this.cbxBoots.Name = "cbxBoots";
             this.cbxBoots.Size = new System.Drawing.Size(121, 21);
             this.cbxBoots.TabIndex = 9;
             this.cbxBoots.SelectedIndexChanged += new System.EventHandler(this.cbxBoots_SelectedIndexChanged);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(790, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.refreshToolStripMenuItem.Text = "&Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Text = "&Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.barProgress});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 547);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(790, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(673, 17);
+            this.lblStatus.Spring = true;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // barProgress
+            // 
+            this.barProgress.Name = "barProgress";
+            this.barProgress.Size = new System.Drawing.Size(100, 16);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 535);
+            this.ClientSize = new System.Drawing.Size(790, 569);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.cbxBoots);
             this.Controls.Add(this.improvementBox);
             this.Controls.Add(this.label1);
@@ -181,10 +256,17 @@
             this.Controls.Add(this.avgBox);
             this.Controls.Add(this.avgLabel);
             this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main";
             this.Text = "Boot Time Diagnostics";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,5 +284,13 @@
         private System.Windows.Forms.TextBox improvementBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbxBoots;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripProgressBar barProgress;
     }
 }
--- BootTimeCheck/Main.cs
+++ BootTimeCheck/Main.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +16,17 @@ namespace BootTimeCheck
     {
         public Main(EventLogInfoReader eL)
         {
-            EventLogInfoReader eventLog = eL;
+            this.eventLog = eL;
             InitializeComponent();
             Random rnd = new Random();
 
             this.data = eL.ReadEventLogData();
             cbxBoots.SelectedIndex = 0;
+
+            this.printPreviewDialog = new PrintPreviewDialog();
+            this.printDocument = new PrintDocument();
+            this.pageSetupDialog = new PageSetupDialog();
+            printDocument.PrintPage += new PrintPageEventHandler(printDocument_PrintPage);
         }
 
         private void drawData(int eventCount)
@@ -31,6 +38,7 @@ namespace BootTimeCheck
             minBox.Text = FindMinValue(lastX) / 1000 + "s";
             avgBox.Text = FindMean(lastX) / 1000 + "s";
             improvementBox.Text = FindImprovement(lastX) / 1000 + "s";
+            barProgress.Value = 50;
         }
 
         private int FindMaxValue(List<IEvent> list)
@@ -86,6 +94,7 @@ namespace BootTimeCheck
             //gather data points from EventLogInfoReader
             for (int events = 0; events < list.Count(); events++)
             {
+                barProgress.Value = 0;
                 int bootTime = list.ToArray()[events].BootTime() / 1000;
                 int mainPath = list.ToArray()[events].MainPath() / 1000;
                 int postBoot = list.ToArray()[events].PostBoot() / 1000;
@@ -104,6 +113,7 @@ namespace BootTimeCheck
                         color = Color.LimeGreen;
                         break;
                 }
+                barProgress.Value = 50;
 
                 createPoint(events, bootTime, color, list);
 
@@ -119,6 +129,7 @@ namespace BootTimeCheck
                 //}
 
                 chart1.Series["Series"].Points[events].AxisLabel = list.ToArray()[events].DateTime().Date.ToShortDateString();
+                barProgress.Value = 100;
             }
         }
 
@@ -139,7 +150,17 @@ namespace BootTimeCheck
             }
         }
 
-        private void cbxBoots_SelectedIndexChanged(object sender, System.EventArgs e)
+        private void refreshData()
+        {
+            barProgress.Value = 0;
+            lblStatus.Text = "Refreshing...";
+            this.data = this.eventLog.ReadEventLogData();
+            barProgress.Value = 25;
+            showData();
+            lblStatus.Text = "Data Refreshed!";
+        }
+
+        private void showData()
         {
             int[] math = new int[6];
             math[0] = 2;
@@ -149,15 +170,124 @@ namespace BootTimeCheck
             math[4] = 21;
             math[5] = Int32.MaxValue;
 
+            barProgress.Value = 30;
+
             int count = math[cbxBoots.SelectedIndex];
 
             if (count > data.Count)
             {
                 count = data.Count;
             }
+
             drawData(count);
+            barProgress.Value = 100;
+            lblStatus.Text = "Data loaded!";
+        }
+
+        private void cbxBoots_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            showData();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            refreshData();
+        }
+
+        private void printDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Font printFont = new Font("Segoe UI", 12);
+            Font titleFont = new Font("Segoe UI", 18, FontStyle.Bold);
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            Rectangle titleRect = new Rectangle(e.MarginBounds.Left, e.MarginBounds.Top, e.MarginBounds.Width, 40);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Near;
+
+            e.Graphics.DrawString("Boot Time Report", titleFont, Brushes.Black, titleRect, format);
+
+
+            Rectangle rect = new Rectangle(e.MarginBounds.Left, e.MarginBounds.Top + 40, e.MarginBounds.Width, e.MarginBounds.Height/2);
+            chart1.Printing.PrintPaint(e.Graphics, rect);
+            int lineLocation = e.MarginBounds.Top + e.MarginBounds.Height/2 + 50;
+            Rectangle pageBounds = new Rectangle(e.MarginBounds.Left, lineLocation, e.MarginBounds.Width, e.MarginBounds.Height / 2);
+
+            int pointCount = (chart1.Series["Series"].Points.Count - 1);
+            String pointWord = (pointCount == 1) ? "boot" : "boots";
+
+            double percentageDifference = chart1.Series["Series"].Points[chart1.Series["Series"].Points.Count-1].YValues[0] / chart1.Series["Series"].Points[0].YValues[0];
+            percentageDifference = 100 - Math.Round(percentageDifference, 2) * 100;
+
+            String statsTitleString = "Statistics for last " + pointCount + " " + pointWord + ":\n";
+            Font statsTitleFont = new Font("Segoe UI", 14, FontStyle.Bold | FontStyle.Underline);
+
+            String statsString = "Average boot time: " + avgBox.Text + "\n" +
+                                 "Minimum boot time: " + minBox.Text + "\n" +
+                                 "Maximum boot time: " + maxBox.Text + "\n" +
+                                 "Time savings: " + percentageDifference + "%\n" +
+                                 "Time savings (in seconds): " + improvementBox.Text + "\n";
+
+            e.Graphics.DrawString(statsTitleString, statsTitleFont, Brushes.Black, pageBounds, format);
+            pageBounds.Offset(0, 25);
+            e.Graphics.DrawString(statsString, printFont, Brushes.Black, pageBounds, format);
+
+            System.Reflection.Assembly myAss = System.Reflection.Assembly.GetExecutingAssembly();
+            if (myAss.GetManifestResourceNames().Contains("BootTimeCheck.logo.png"))
+            {
+                Stream myStream = myAss.GetManifestResourceStream("BootTimeCheck.logo.png");
+                Bitmap logo = new Bitmap(myStream);
+                Rectangle logoRect = new Rectangle(e.MarginBounds.Right - logo.Width / 5, e.MarginBounds.Bottom - logo.Height / 5, logo.Width / 5, logo.Height / 5);
+                e.Graphics.DrawImage(logo, logoRect);
+            }
+        }
+        
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog.Document = printDocument;
+            printPreviewDialog.SetDesktopBounds(100, 100, 600, 800);
+            printPreviewDialog.ShowDialog();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.P))
+            {
+                printPreviewDialog.Document = printDocument;
+                printPreviewDialog.ShowDialog();
+                return true;
+            }
+
+            if (keyData == (Keys.Control | Keys.X))
+            {
+                Application.Exit();
+                return true;
+            }
+
+            if (keyData == Keys.F5)
+            {
+                refreshData();
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
         }
 
         public List<IEvent> data { get; set; }
+
+        public EventLogInfoReader eventLog { get; set; }
+
+        public PrintPreviewDialog printPreviewDialog { get; set; }
+
+        public PrintDocument printDocument { get; set; }
+
+        public PageSetupDialog pageSetupDialog { get; set; }
     }
 }
